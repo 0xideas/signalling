de8061e5021810f025520b40a4a149763e468438
leon_git <leontimnaluithlen@gmail.com>
2018-10-12 12:43:55 +0200

    separated act_all and update_all

--- environment.py
+++ environment.py
@@ -15,6 +15,7 @@ class Environment(object):
 		self.fields = [(x,y) for x in range(dimensions[0]) for y in range(dimensions[1])]
 		agent_location_id = [choice(np.arange(dimensions[0]*dimensions[1]), replace=False) for x in range(agent_number)]
 		self.agent_locations = [self.fields[x] for x in agent_location_id]
+		self.agent_actions = ["None" for x in self.agents]
 
 	def move_all(self):
 		for agent_id in self.agents:
@@ -49,12 +50,21 @@ class Environment(object):
 			x += 1
 		print(board)
 
-	def act_update_all(self):
+	def act_all(self):
+		self.agent_actions = [str("None") for x in self.agent_actions]
 		for agent_id in self.agents:
+			agent_i = int(agent_id[1:])
 			agent = self.agents[agent_id]
 			state = self.determine_state(agent_id)
 			action = agent.act(state)
 
+			self.agent_actions[agent_i] = action
+
+	def update_all(self):
+		for agent_id in self.agents:
+			agent = self.agents[agent_id]
+			state = agent.last_state
+			action = agent.last_action
 			if (state == "s0" and action == "a0"):
 				agent.update(1)
 			elif (state == "s1" and action == "a1"):
@@ -79,10 +89,11 @@ if __name__ == '__main__':
 	env = Environment((5,5), 5, 2, 2)
 	env.print_board()
 
-	for x in range(100):
+	for x in range(200):
 		print('_______________ step {} ________________'.format(x))
 		env.move_all()
-		env.act_update_all()
+		env.act_all()
+		env.update_all()
 		env.print_board()
 
 
