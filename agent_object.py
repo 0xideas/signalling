d97824df0de57e84ee412e7d04bf6c53ee6dc851
leon_git <leontimnaluithlen@gmail.com>
2018-10-12 12:31:29 +0200

    finding neighbours

---
+++ agent_object.py
@@ -0,0 +1,87 @@
+from numpy.random import choice
+
+class Agent(object):
+	def __init__(self, action_number=2, state_number=2, alpha=0.2):
+		self.action_number = action_number
+		self.state_number = state_number
+		self.alpha = alpha
+		self.last_action = "None"
+		self.last_state = "None"
+		self.actions = ["a" + str(x) for x in range(action_number)]
+		self.action_rewards = {"s" + str(y):[1 for x in range(action_number)] for y in range(state_number)}
+		self.action_choice = self.new_action_prob()
+		self.last_movement = "None"
+		self.movements = ["up", "down", "left", "right", "stay"]
+		self.movement_choice = [0.2, 0.2, 0.2, 0.2, 0.2]
+
+	def move(self):
+		self.last_movement = choice(self.movements, p=self.movement_choice)
+		return(self.last_movement)
+
+
+	def act(self, state):
+		self.last_state = state
+		action_probs = self.action_choice[state]
+		self.last_action = choice(self.actions, p=action_probs)
+		return(self.last_action)
+
+	def update(self, reward):
+		rewards = self.action_rewards[self.last_state]
+		action = int(self.last_action[1])
+		rewards[action] = reward * self.alpha + rewards[action] * (1 - self.alpha)
+		self.action_rewards[self.last_state] = rewards
+		self.action_choice = self.new_action_prob()
+		self.movement_choice = self.new_movement_prob(reward)
+
+	def new_movement_prob(self, reward):
+		last_move_i = self.movements.index(self.last_movement)
+		last_prob = self.movement_choice[last_move_i]
+		new_prob = last_prob + self.alpha/2 * (reward - 1)
+		move_choice = self.movement_choice
+		move_choice[last_move_i] = new_prob
+		move_choice = [max(0, x) for x in move_choice]
+		total_prob = sum(move_choice)
+		return([move_choice[x]/total_prob for x in range(5)])
+
+
+	def new_action_prob(self):
+		action_probs = {}
+		for i in range(self.state_number):
+			state_i = "s" + str(i)
+			total_reward = sum(self.action_rewards[state_i])
+			action_probs[state_i] = [self.action_rewards[state_i][x]/total_reward for x in range(self.action_number)]
+		return(action_probs)
+
+
+if __name__ == "__main__":
+	ag = Agent(2,3)
+	ag.act("s1")
+
+	correct_response = []
+
+	for x in range(100):
+		move = ag.move()
+		state = choice(["s0", "s1", "s2"])
+		action = ag.act(state)
+
+		if state == "s0":
+			if action == "a0":
+				ag.update(3)
+			else:
+				ag.update(0)
+		if state == "s1":
+			if action == "a0":
+				ag.update(0)
+			else:
+				ag.update(3)
+
+		print("-----------------------------------------------------")
+		print("move: ", move)
+		print("state: ", state)
+		print("action: ", action)
+		print("movement choice: ")
+		print(ag.movement_choice)
+		print("action rewards: ")
+		print(ag.action_rewards)
+		print("action _choice: ")
+		print(ag.action_choice)
---
+++ environment.py
@@ -0,0 +1,88 @@
+from agent_object import Agent
+from numpy.random import choice
+import numpy as np
+
+
+class Environment(object):
+	def __init__(self, dimensions, agent_number, action_number, state_number):
+		assert dimensions[0] * dimensions[1] >= agent_number
+		self.dimensions = dimensions
+		self.agent_number = agent_number
+		self.action_number = action_number
+		self.state_number = state_number
+		self.states = ["s" + str(x) for x in range(state_number)]
+		self.agents = {"A" + str(x): Agent(action_number, state_number) for x in range(agent_number)}
+		self.fields = [(x,y) for x in range(dimensions[0]) for y in range(dimensions[1])]
+		agent_location_id = [choice(np.arange(dimensions[0]*dimensions[1]), replace=False) for x in range(agent_number)]
+		self.agent_locations = [self.fields[x] for x in agent_location_id]
+
+	def move_all(self):
+		for agent_id in self.agents:
+			agent = self.agents[agent_id]
+			move = agent.move()
+			agent_i = int(agent_id[1:])
+			new_loc = self.move_agent(self.agent_locations[agent_i], move)
+			self.agent_locations[agent_i] = new_loc
+
+	def move_agent(self, original_location, move):
+		location = tuple(original_location)
+		if move == "up":
+			location = (location[0] - 1, location[1])
+		elif move == "down":
+			location = (location[0] + 1, location[1])
+		elif move == "left":
+			location = (location[0], location[1] - 1)
+		elif move == "right":
+			location = (location[0], location[1] + 1)
+
+		if 0 <= location[0] < self.dimensions[0] and 0 <= location[1] < self.dimensions[1] and location not in self.agent_locations:
+			return location
+		else:
+			return tuple(original_location)
+
+	def print_board(self):
+		board = np.zeros(self.dimensions)
+		x = 0
+		for location in self.agent_locations:
+			#one cannot see the first agent
+			board[location] = x
+			x += 1
+		print(board)
+
+	def act_update_all(self):
+		for agent_id in self.agents:
+			agent = self.agents[agent_id]
+			state = self.determine_state(agent_id)
+			action = agent.act(state)
+
+			if (state == "s0" and action == "a0"):
+				agent.update(1)
+			elif (state == "s1" and action == "a1"):
+				agent.update(2)
+			else:
+				agent.update(0)
+
+	def determine_state(self, agent_id):
+		#this version determines whether the agent has a neighbour
+		agent_i = int(agent_id[1:])
+		state = "s0"
+		agent_location = self.agent_locations[agent_i]
+		for location in self.agent_locations:
+			cond1 = (abs(location[0] - agent_location[0]) == 1) and (location[1] == agent_location[1])
+			cond2 = (abs(location[1] - agent_location[1]) == 1) and (location[0] == agent_location[0])
+			if location != agent_location and (cond1 or cond2):
+				state = "s1"
+		return(state)
+
+
+if __name__ == '__main__':
+	env = Environment((5,5), 5, 2, 2)
+	env.print_board()
+
+	for x in range(100):
+		print('_______________ step {} ________________'.format(x))
+		env.move_all()
+		env.act_update_all()
+		env.print_board()
+
+
